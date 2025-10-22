8a62f9b28e39acaaf0f0653696789f6d57042fa8
Chris Martin <chris.martin@renaissance.com>
2025-10-22 12:20:49 -0600

    build(nix): add aarch64-darwin

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs.nixpkgs.follows = "haskellNix/nixpkgs-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
   outputs = { self, nixpkgs, flake-utils, haskellNix }:
-    flake-utils.lib.eachSystem [ "x86_64-linux" "x86_64-darwin" ] (system:
+    flake-utils.lib.eachSystem [ "x86_64-linux" "x86_64-darwin" "aarch64-darwin" ] (system:
     let
       overlays = [ haskellNix.overlay
         (final: prev: {
